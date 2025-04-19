ef5ac1415e1d55782013107736ea37cb6921fbd4
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-04-19 22:44:39 +0530

    Created DBConnection class to manage database connections

---
+++ DBConnection.java
@@ -0,0 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// This class is responsible for creating a connection to the MySQL database
+public class DBConnection {
+
+}
