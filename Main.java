41407c58ccf6505037dc985985255c664788e745
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-04-19 23:04:01 +0530

    Implement InvalidChoiceException Class

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Name: Sudhanshu Kulkarni
+// PRN: 23070126132
+// Batch: AIML-B3
+
+class InvalidChoiceException extends Exception {
+    public InvalidChoiceException(String message) {
+        super(message);
+    }
+}
+
